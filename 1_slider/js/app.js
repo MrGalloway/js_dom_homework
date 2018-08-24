3eba7786ee7da2118ab9cfaae6f73e5e1bca1f78
throwaway667 <f18.logx@gmail.com>
2018-08-24 17:23:16 +0200

    slider done

--- 1_slider/js/app.js
+++ 1_slider/js/app.js
@@ -1,3 +1,42 @@
-/**
- * Created by Jacek on 2015-12-16.
- */
+document.addEventListener('DOMContentLoaded', function (){
+	console.log('xD');
+
+	//punkt2
+	const prevButton = document.querySelector('#prevPicture');
+	const nextButton = document.querySelector('#nextPicture');
+	const listElements = document.querySelectorAll('.slider li');
+	const slider = document.querySelector('.slider');
+	
+	let pictureIndex = 0;
+
+	console.log(prevButton);
+	console.log(nextButton);
+	console.log(listElements);
+	console.log(slider);
+	console.log(pictureIndex);
+	console.log(isNaN(pictureIndex));
+	
+	//punkt3
+	listElements[pictureIndex].classList += 'visible';
+
+	//punkt4
+	prevButton.addEventListener('click', prevClickHandler); 
+	nextButton.addEventListener('click', nextClickHandler);
+
+	function prevClickHandler(){
+		if(pictureIndex>0){
+			getVisiblePic().classList.remove('visible');
+			pictureIndex--;
+			listElements[pictureIndex].className = 'visible';
+		}
+	};
+	function nextClickHandler(){
+		if(pictureIndex<listElements.length-1){
+			getVisiblePic().classList.remove('visible');
+			pictureIndex++;
+			listElements[pictureIndex].className = 'visible';
+		}
+	};
+	
+	function getVisiblePic(){return document.querySelector('.slider .visible')};
+});
