db3d19b4b5d8413e22588da9110d65ec8dfb724b
throwaway667 <f18.logx@gmail.com>
2018-08-25 16:35:31 +0200

    menu done

--- 4_menu/js/app.js
+++ 4_menu/js/app.js
@@ -1,3 +1,35 @@
 /**
  * Created by Jacek on 2015-12-16.
  */
+document.addEventListener('DOMContentLoaded',function (){
+	console.log('xD');
+
+	const mainListElements = document.querySelectorAll('div > ul > li');
+	console.log(mainListElements);
+
+	document.querySelector('div').className="nav";
+
+	for(element of mainListElements){
+		console.log(element);
+		element.addEventListener('mouseover',mouseOverHandler);
+		element.addEventListener('mouseout',mouseOutHandler);
+	}
+
+	function mouseOverHandler(evt){
+		if(evt.target.querySelector('ul') !== null){
+			console.log(evt.target);
+			console.log(evt.target.querySelector('ul'));
+			evt.target.querySelector('ul').style.display = 'block';
+		}
+	};
+	function mouseOutHandler(evt){
+		if(evt.target.querySelector('ul') !== null){
+			console.log('Mouse out');
+			evt.target.className='';
+			evt.target.querySelector('ul').style.display = 'none';
+		}
+	};
+});
+
+//problem:
+//nie da sie najechac na elementy submenu
