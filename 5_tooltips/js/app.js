21b9eaf527ea78c5aa242007138680c74c02f2ba
throwaway667 <f18.logx@gmail.com>
2018-08-25 17:11:26 +0200

    tooltips done

--- 5_tooltips/js/app.js
+++ 5_tooltips/js/app.js
@@ -6,3 +6,40 @@
 /*
  <span class="tooltipText">Text tooltipa</span>
  */
+
+document.addEventListener('DOMContentLoaded',function (){
+	console.log('xD');
+
+	const tooltips = document.querySelectorAll('.tooltip');
+	console.log(tooltips);
+
+	for(tooltip of tooltips){
+		console.log(tooltip);
+		tooltip.addEventListener('mouseover',mouseOverHandler);
+		tooltip.addEventListener('mouseout',mouseOutHandler);
+	}
+
+	function mouseOverHandler(evt){
+		const tooltipText = evt.target.attributes['data-text'].value;
+		console.log(tooltipText);
+		evt.target.appendChild(createSpanWithText(tooltipText));
+	};
+	
+	function mouseOutHandler(evt){
+		const tooltip = evt.target.querySelector('.tooltipText');
+		console.log(tooltip);
+		evt.target.removeChild(tooltip);
+	};
+
+	function createSpanWithText(text){
+		const span = document.createElement('span');
+		span.className = 'tooltipText';
+		span.textContent = text;
+
+		return span;
+		
+	};
+});
+
+//problem:
+//tooltipy znikaja za szybko
